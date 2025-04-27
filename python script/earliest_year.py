599d81ee16d6c626933c045f4b8d062691e06a4b
linnypham <89488749+linnypham@users.noreply.github.com>
2025-04-27 02:12:08 -0400

    Update earliest_year.py

--- python script/earliest_year.py
+++ python script/earliest_year.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('baseball-reference data\hof-pitching.csv')
-smallest_year = df['From'].min()
-largest_year = df['From'].max()
+df = pd.read_csv('baseball-reference data/all_batting_updated.csv')
+smallest_year = df['Season'].min()
+largest_year = df['Season'].max()
 print(f'Smallest: {smallest_year}')
 print(f'Largest: {largest_year}')
